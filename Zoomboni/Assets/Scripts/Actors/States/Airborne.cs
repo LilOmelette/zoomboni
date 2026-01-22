c163df0f1a9d30a91e7216df02a6adefb99ce38a
Bynine <byninegiga@gmail.com>
2026-01-22 16:46:14 -0500

    Fixed setup

--- Zoomboni/Assets/Scripts/Actors/States/Airborne.cs
+++ Zoomboni/Assets/Scripts/Actors/States/Airborne.cs
@@ -10,7 +10,6 @@ public class Airborne : State
     [SerializeField] private float GRAVITY = 1.0f;
     [SerializeField] private float ACCELERATION = 1.0f;
     [SerializeField] private float FRICTION = 1.0f;
-    [SerializeField] private float RADIUS_CHECK_WALL = 1.0f;
     public override void Enter(Component statePrior)
     {
         sfxLoop.Play();
@@ -33,12 +32,6 @@ public class Airborne : State
     public override void PhysicsUpdate()
     {
         Vector3 velocity = new Vector3(player.cc.velocity.x, player.cc.velocity.y, player.cc.velocity.z);
-
-        //if (Physics.CheckSphere(player.transform.position, RADIUS_CHECK_WALL) && velocity.magnitude < 1)
-        //{
-        //    print("Push away push away");
-        //    velocity -= player.directionContainerForModel;
-        //}
 
         velocity = ApplyGravity(velocity, GRAVITY);
         velocity = ApplyAcc(velocity, ACCELERATION);
